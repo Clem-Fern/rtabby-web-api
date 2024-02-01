adb1063528951c3fc272070d8b7208e6be3c8b7f
Clem Fern <clemfern456@gmail.com>
2024-02-01 15:20:12 +0100

    ref/fix: auth middleware (handling error)

--- src/auth.rs
+++ src/auth.rs
@@ -1,42 +1,57 @@
-use actix_web::{web, dev::ServiceRequest, Error, error::ErrorUnauthorized};
+use crate::app_config::MappedAppConfig;
+use actix_web::{dev::ServiceRequest, error::ErrorUnauthorized, web, Error};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use log::warn;
-use crate::app_config::MappedAppConfig;
-cfg_if! {
-    if #[cfg(feature = "third-party-login")] {
-        use crate::storage::DbPool;
-        use crate::login::models::User;
-    }
-}
 
-pub async fn bearer_auth_validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+pub async fn bearer_auth_validator(
+    req: ServiceRequest,
+    credentials: BearerAuth,
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
     let default = web::Data::new(MappedAppConfig::default());
-    let users: &Vec<String> = &req.app_data::<web::Data<MappedAppConfig>>().unwrap_or(&default).users.clone().into_keys().collect();
-    let token = String::from(credentials.token()); 
-    #[cfg(feature = "third-party-login")]
-    let mtoken = token.clone();
+    let users: &Vec<String> = &req
+        .app_data::<web::Data<MappedAppConfig>>()
+        .unwrap_or(&default)
+        .users
+        .clone()
+        .into_keys()
+        .collect();
+    
+    let token = String::from(credentials.token());
+    
     if users.contains(&token) {
-        Ok(req)
-    } else {
-        #[cfg(feature = "third-party-login")]
+        return Ok(req);
+    }
+
+    #[cfg(feature = "third-party-login")]
+    {
+        use crate::login::models::User;
+        use crate::storage::DbPool;
+        use actix_web::error::ErrorInternalServerError;
+
+        let pool = req.app_data::<web::Data<DbPool>>().unwrap().clone();
+        let token = token.clone();
+
+        match web::block(move || {
+            let mut conn = pool.get()?;
+            User::get_user_by_token(&mut conn, &token)
+        })
+        .await
         {
-            let pool = req.app_data::<web::Data<DbPool>>().unwrap().clone(); 
-            if let Some(_user) = web::block(move || { 
-                let mut conn = pool.get()?; 
-                User::get_user_by_token(&mut conn, &mtoken) 
-            })     
-            .await.unwrap().unwrap() { 
-                Ok(req) 
-            } else { 
-                warn!("Authentification failed for {:?}", req.connection_info().peer_addr()); 
-                Err((ErrorUnauthorized("Invalide authentication token !"), req)) 
-            } 
+            Ok(result) => match result {
+                Ok(result) => {
+                    if let Some(_user) = result {
+                        return Ok(req);
+                    }
+                }
+                Err(err) => return Err((ErrorInternalServerError(err), req)),
+            },
+            Err(err) => return Err((ErrorInternalServerError(err), req)),
         }
-        
-        #[cfg(not(feature = "third-party-login"))]
-        {
-            warn!("Authentification failed for {:?}", req.connection_info().peer_addr());
-            Err((ErrorUnauthorized("Invalide authentication token !"), req))
-        }
-    }    
+    }
+
+    warn!(
+        "Authentification failed for {:?}",
+        req.connection_info().peer_addr()
+    );
+    Err((ErrorUnauthorized("Invalide authentication token !"), req))
 }
